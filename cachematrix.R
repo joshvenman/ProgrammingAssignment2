7c65fc716479d0513240a797ffbda0a70200b5be
Josh Venman <joshvenman@gmail.com>
2015-01-24 18:04:23 +1100

    Final version Assignment 2
    Final version of makeCacheMatrix and cacheSolve functions for R
    Programming - Assignment 2 - Caching the Inverse of a Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,64 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+## cacheSolve: This function computes the inverse of the special "matrix" returned 
+## by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 
-## Write a short comment describing this function
+## makeCacheMatrix: create a special "matrix" that contains functions to set and get the value 
+## of the matrix and set and get the value of the inverse of the matrix passed
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { 
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) inv <<- solve
+        getinv <- function() inv
+        getenv<- function() environment()
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv,
+             getenv = getenv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve: get the inverse of the passed matrix unless the passed matrix is unchanged from the previous call
+## if it has not changed, use the cached inverse value. If it has changed, compute the new inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+        m <- x$getinv()
+        env <- x$getenv()
+        data <- x$get()
+        matrixChanged = FALSE
+
+        # if no current saved last matrix then make it an emoty matrix and force solve()
+        if(!exists("lastmatrix")) {
+                lastmatrix <- matrix()
+                matrixChanged <- TRUE
+        }
+
+        # Has the source matrix changed?
+        if(identical(lastmatrix, data)) {
+                # Matrix has not changed - get existing
+                message("Matrix has not changed")
+                matrixChanged <- FALSE
+        } else {
+                message("Matrix has changed")
+                matrixChanged <- TRUE
+        }
+        
+        if(!is.null(m)) {
+                # Have an existing cached inverse so if matrix has not changed, use this
+                if(!matrixChanged) {
+                        message("Getting cached inverse")
+                        return(m)
+                }
+        }
+        
+        m <- solve(data, ...)
+        x$setinv(m)
+        # Persist this matrix for comparison next time
+        lastmatrix <<- data
+        m
 }
